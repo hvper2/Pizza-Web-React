5741ed5038c84cfab74fe2fb123b16070a2a9ca0
Mariusz <mariusz.kwiecien2@onet.pl>
2023-06-13 21:38:14 +0200

    Pizza Web

---
+++ pizza-web/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ pizza-web/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function home() {
+  return <div>Hello World</div>;
+}
+
+export default home;
